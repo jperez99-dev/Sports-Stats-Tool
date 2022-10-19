cdcfdc7458624f865f8ae9af38e13292ad0aa7ed
Jordan <jordan.perez01@gmail.com>
2022-10-19 16:06:25 -0500

    Configured main menu and set up structure for data cleanup function.

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
 import constants
+
+def main():
+    def cleandata():
+        cleanplayerdata = {}
+
+
+
+    def display():
+        loop = True
+        print("""BASKETBALL TEAM STATS TOOL
+
+---- MENU ----
+
+OPTIONS:
+A) Display Team Stats
+B) Quit""")
+        while loop:
+            userInput = input("Enter an option: ")
+            if userInput == 'a' or userInput.upper() == 'A':
+                cleandata()
+                loop = False
+            elif userInput == 'b' or userInput.upper() == 'B':
+                quit()
+            else: 
+                print("Error please enter the options \"A\" or \"B\"")
+
+    
+        
+        
+        
+    display()
+
+
+if __name__ == "__main__":
+    main()
