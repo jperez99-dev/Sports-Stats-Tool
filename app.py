fa7efef2b278f282786451d2ad15a4fbc466d436
Jordan <jordan.perez01@gmail.com>
2022-10-23 04:35:08 -0500

    Clean data function now has logic that cleans data from constants.py and adds elements to empty list of dicionaries that will  be used to display to the user when more features are complete.

--- app.py
+++ app.py
@@ -2,10 +2,29 @@ import constants
 
 def main():
     def cleandata():
-        cleanplayerdata = {}
+        """
+        This logic for the cleandata function loops through all elements in constants.py and determines if data needs to be cleaned,
+        if data doesnt need to be cleaned it will copy the value from constants.py into empty cleanPlayerData list of dictionaries.
+        """
+        playerCount = 0
+        for i in range(len(constants.PLAYERS)):
+            playerCount = playerCount + 1
 
-
-
+        cleanedPlayerData = [{} for i in range(playerCount)]
+  
+        for i in range(len(constants.PLAYERS)):
+            for j in range(len(constants.PLAYERS[i])):
+                if constants.PLAYERS[i][list(constants.PLAYERS[i].keys())[j]] == 'YES':
+                    cleanedPlayerData[i][list(constants.PLAYERS[i].keys())[j]] = True
+                elif constants.PLAYERS[i][list(constants.PLAYERS[i].keys())[j]] == 'NO':
+                    cleanedPlayerData[i][list(constants.PLAYERS[i].keys())[j]] = False
+                elif list(constants.PLAYERS[i].keys())[j] == 'height':
+                    height = constants.PLAYERS[i][list(constants.PLAYERS[i].keys())[j]]
+                    heightSplit = height.split(' ')
+                    cleanedPlayerData[i][list(constants.PLAYERS[i].keys())[j]] = int(heightSplit[0])
+                else:
+                    cleanedPlayerData[i][list(constants.PLAYERS[i].keys())[j]] = constants.PLAYERS[i][list(constants.PLAYERS[i].keys())[j]]
+        print(cleanedPlayerData)
     def display():
         loop = True
         print("""BASKETBALL TEAM STATS TOOL
